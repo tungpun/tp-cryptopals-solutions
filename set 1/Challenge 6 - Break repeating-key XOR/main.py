0816ab29e918662aa2f1c4fc3b3a19f20d39df73
Nguyen Huu Tung <htung.nht@gmail.com>
2015-04-18 11:49:20 +0700

    Completed challenge 6, set 1

--- set 1/Challenge 6 - Break repeating-key XOR/main.py
+++ set 1/Challenge 6 - Break repeating-key XOR/main.py
@@ -8,6 +8,7 @@ import base64
 import re
 from random import randint
 
+MAXINT = 10000
 
 def strHamming(s1, s2):
 	difCnt = 0
@@ -47,6 +48,8 @@ def min2(a, b):
 def find_key_size(data):	
 	print "=================== FIND KEY SIZE ==================="	
 	n = len(data)
+	minkeysize = MAXINT
+	minkey = -1
 	for keysize in range(2, min2(n/2, 35)):		
 		
 		p = []
@@ -59,32 +62,55 @@ def find_key_size(data):
 			first = p[randint(0, len(p)-1)]
 			second = p[randint(0, len(p)-1)]			
 			sumHammingDistance += float(binHamming(first, second) / keysize)								
+
 		print keysize, sumHammingDistance / cntrepeated
 
+		if sumHammingDistance / cntrepeated < minkeysize:
+			minkeysize = sumHammingDistance / cntrepeated
+			minkey = keysize
+	return minkey
 
+
+def is_prinable(r):
+	return (r == 10) or (32 <= r < 128)	
+
+
+def find_key(keysize, data):
+	print "=================== FIND KEY ==================="	
+	blocks = re.findall('.'*keysize*2, data)	
+	
+	key = ''
+
+	for ikey in range(0, keysize):		# Find key character at position ikey
+		print "Key[" + str(ikey) + "] = ",
+		for i in range(10, 128):		# Try all posible of key char, which is prinable. You can expand it
+			keychar = chr(i)		
+			ok = 0	
+
+			for block in blocks:			
+				block = block.decode('hex')				
+				r = ord(block[ikey]) ^ ord(keychar)				
+				if is_prinable(r):			
+					ok += 1						
+			if len(blocks) - ok == 0:						
+				print '[' + keychar + ']', 
+				
+		print ''
 
 
 def main():
-	f = open("7.txt", "r")
+	f = open("6.txt", "r")
 	data = f.read()
 	f.close()
-	data = base64.b64decode(data) 				
-	print len(data)
-	# find_key_size(data)
-	keysize = 3
-	blocks = re.findall('.'*keysize, data)
+	data = base64.b64decode(data).encode('hex') 				
+	
+	keySize = find_key_size(data)
+	keysize = 29
+	print "Keysize = " + str(keysize)	
+	
 
-	for i in range(1, 256):
-		keychar = chr(i)		
-		ok = 0	
-		for block in blocks:			
-			r = ord(block[0]) ^ ord(keychar)
-			if 32 <= r <= 160:				
-				ok += 1				
-		if len(blocks) - ok == 0:		
-			print keychar, i, len(blocks) - ok
-
-
+	find_key(keysize, data)
+					
 
 if __name__ == '__main__':
 	main()
